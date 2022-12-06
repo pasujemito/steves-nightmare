94280089f903ac30eb82ad3122bdb350f04fd4f0
sofiyakryu <63737183+sofiyakryu@users.noreply.github.com>
2022-12-06 14:54:50 +0000

    create createTable function

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -21,32 +21,60 @@ const Restaurant = ({ steak, vegan }) => {
         return denominator
     }
 
-    const steakTableCount = denomination(steak.office, steak.remote)
-    const veganTableCount = denomination(vegan.office, vegan.remote)
+    // const steakTableCount = denomination(steak.office, steak.remote)
+    // const veganTableCount = denomination(vegan.office, vegan.remote)
 
-
-    const arr1 = steak.office.slice(steak.office.length / steakTableCount);
-    const arr2 = steak.remote.slice(steak.remote.length / steakTableCount);
-    
-    const merge = (first, second) => {
-        for (let i = 0; i < second.length; i++) {
-            first.push(second[i]);
+    const createTable = (group) => {
+        let tableCount = denomination(group.office, group.remote);
+        let remote = group.remote.slice(group.remote.length / tableCount);
+        let office = group.office.slice(group.office.length / tableCount);
+        const merge = (first, second) => {
+            for (let i = 0; i < second.length; i++) {
+                first.push(second[i]);
+            }
+            return first;
         }
-        return first;
+        
+        return merge(remote, office);
     }
 
-    const guests = merge(arr1, arr2);
+    const guestsBatch1 = createTable(steak);
+    const guestsBatch2 = createTable(vegan);
 
-    console.log(arr1);
-    console.log(arr2);
-    console.log(merge(arr1, arr2));
+    // const arr1 = steak.office.slice(steak.office.length / steakTableCount);
+    // const arr2 = steak.remote.slice(steak.remote.length / steakTableCount);
+    
+    // const merge = (first, second) => {
+    //     for (let i = 0; i < second.length; i++) {
+    //         first.push(second[i]);
+    //     }
+    //     return first;
+    // }
+
+    // const guests = merge(arr1, arr2);
+
+
+    // console.log(arr1);
+    // console.log(arr2);
+    // console.log(merge(arr1, arr2));
 
     return (
         <>
             <div className="restaurant">
                 <div className="table">
                     <p>Steak</p>
-                    {guests.map(guest => {
+                    {guestsBatch1.map(guest => {
+                        return (
+                            <div className="guest">
+                                <p>{guest.first_name}</p>
+                            </div>
+                        )
+                    })
+                    }
+                </div>
+                <div className="table">
+                    <p>Vegan</p>
+                    {guestsBatch2.map(guest => {
                         return (
                             <div className="guest">
                                 <p>{guest.first_name}</p>
